43c52c8e112a0f747d04658d67c58c1a7727589d
Isidro Urriza Parroqué <urriza@unizar.es>
2026-02-04 13:55:54 +0100

    Add comments and TODOes to lab.vhd

--- rtl/lab2.vhd
+++ rtl/lab2.vhd
@@ -1,18 +1,37 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+-- ==============================================================
+--  Lab 2 - VHDL Template
+--  Descripción: Esqueleto de la entidad y arquitectura.
+--  TODO: Completar descripción funcional del diseño.
+-- ==============================================================
 
+-- TODO: Añadir librerías necesarias.
 
 entity lab2 is
     port (
+        -- TODO: Añadir puertos de entrada/salida (señales, tipos, anchos).
 
     );
 end entity lab2;
 
 architecture RTL of lab2 is
 
+    -- ==========================================================
+    --  Declaraciones internas
+    -- ==========================================================
+    -- TODO: Declarar componentes .
+    -- TODO: Declarar señales internas.
+    -- TODO: Declarar constantes y tipos.
 
 begin
 
+    -- ==========================================================
+    --  Instancias
+    -- ==========================================================
+    -- TODO: Instanciar submódulos/componentes.
+
+    -- ==========================================================
+    --  Lógica combinacional
+    -- ==========================================================
+    -- TODO: Implementar el modelo con bloques combinacionales.
 
 end architecture;
